62147c7d310a9cb1492bf8af7f7327d253bc087e
Juliana Auzier Seixas <juliana.auzier.s@gmail.com>
2021-08-05 22:39:01 -0300

    Update esquecisenha.php

--- esquecisenha.php
+++ esquecisenha.php
@@ -1,20 +1,17 @@
 <html lang="pt-br">
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>RESERLOC</title>
-	<script src="js/jquery.min.js"></script>
-	<link rel="stylesheet" href="css/estilo3.css">
-	
-	<style>
-	  .cont{
-		margin-top:20px;
-		border: #f8f8ff;
-	  }
-	</style>
-	
 	<head>
-		
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>RESERLOC</title>
+		<script src="js/jquery.min.js"></script>
+		<link rel="stylesheet" href="css/estilo3.css">	
+		<style>
+	  	    .cont{
+			margin-top:20px;
+			border: #f8f8ff;
+		    }
+		</style>	
 	</head>
 	
 	<body>
@@ -55,9 +52,6 @@
 						</tr>
 					</table>
 			</form><br>
-			<!--<footer class="footer"><br>
-				<p>&copy;2020 - Juliana Auzier</p><br>
-			</footer-->
 		</div>
 	</body>
 </html>
